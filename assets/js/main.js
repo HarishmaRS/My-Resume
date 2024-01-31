7267fe1b1e077ae590845dc5cffd67fee589c906
HarishmaRS <harishmarsuseel@gmail.com>
2024-01-31 13:03:09 +0530

    completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,22 @@
 // Show MENU
-const showMenu = (toggleId, navId) =>{
+const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
-    
+        nav = document.getElementById(navId)
+
     // Validate that variables exist
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
             // We add the show-menu class to the div tag with the nav__menu class
             nav.classList.toggle('show-menu')
         })
     }
 }
-showMenu('nav_toggle','nav_menu')
+showMenu('nav_toggle', 'nav_menu')
 
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav_link')
 
-function linkAction(){
+function linkAction() {
     const navMenu = document.getElementById('nav_menu')
     // When we click on each nav_link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
@@ -26,19 +26,112 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
+function scrollActive() {
     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        const sectionTop = current.offsetTop - 50;
+
+        sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
+        }
+         else {
             document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
+
     })
 }
 window.addEventListener('scroll', scrollActive)
+
+
+
+
+/*==================== SHOW SCROLL TOP ====================*/
+function scrollTop() {
+    const scrollTop = document.getElementById('scroll_top');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if (this.scrollY >= 200) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
+
+
+
+/*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+// REDUCE the size and print on an A4 sheet
+function scaleCv() {
+    document.body.classList.add('scale_cv')
+}
+
+// REMOVE THE SIZE WHEN THE CV IS DOWNLOADED 
+function removeScale() {
+    document.body.classList.remove('scale_cv')
+}
+
+//GENERATE PDF
+//pdf generated area
+let areaCv = document.getElementById('area-cv');
+
+let resumeButton = document.getElementById('resume_button');
+
+//HTML2PDF option
+let option = {
+    margin: 0,
+    filename: 'HarishmaResume.pdf',
+    image: { type: 'jpeg', quality: 0.98 },
+    pagebreak: { mode: ['legacy'] },
+    html2canvas: { scale: 4 },
+    jsPDF: { format: 'a4', orientation: 'portrait' }
+};
+
+//Function to call areaCv and HTML2PDF
+function generateResume() {
+    html2pdf(areaCv, option)
+}
+
+//when the button is clicked, it executes the 3 functions
+resumeButton.addEventListener('click', () => {
+
+
+    //1.the class scale_cv is added to the body, where it reduces the size of elements to a4
+    scaleCv();
+
+    // 2. The PDF is generated
+    generateResume();
+
+    //3. The scale_cv class is removed from the body after 5 seconds to return to normal size after downloaded
+
+    setTimeout(removeScale, 50000)
+})
